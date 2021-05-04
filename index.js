179f69155fe579e563c0018fb7874e28ff96c0d0
robaElshazly <roba1818@gmail.com>
2021-05-04 10:55:56 +1000

    display search list in two search columns

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 
 
-// Get Subscriber Count
-const subCount = document.getElementById('sub-count')
+firstSearchForm=document.getElementById("first-search-form")
+secondSearchForm=document.getElementById("second-search-form")
+firstSearchUl=document.getElementById("first-search-ul")
+secondSearchUl=document.getElementById("second-search-ul")
+//add event listener to left form
+firstSearchForm.addEventListener("submit",function(event){getSearchResult(event,firstSearchUl)})
 
-// get Channel cover Image
-const covImage = document.getElementById('covImage')
+//add event listener to second form
+secondSearchForm.addEventListener("submit",function(event){
+getSearchResult(event,secondSearchUl)})
 
-async function getChannelId() {
-		const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=TheCodingTrain&key=${youtube_key}`)
+
+async function getSearchResult(e,searchUl){
+	e.preventDefault(); 
+	searchBar=e.target.querySelector('input[type=text]')
+	const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${searchBar.value}&key=${youtube-key}`)
 			const data = await response.json()
 			console.log(data)
-			const channelId = await data["items"][0].id.channelId
-	// console.log(channelId)
-	getSubcribers(channelId)
+			// const channelId = await data["items"][0].id.channelId
+			const searchResult = await data["items"]
+	 		// console.log(channelId)
+			// getSubcribers(channelId)
+			showSearchResult(searchResult,searchUl)
 }
-	function getSubcribers(channelId) {
-		fetch(`https://www.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${channelId}&key=${youtube_key}`)
-			.then(response => response.json())
-			.then(data => {
-				
-			let	imgURL = data["items"][0].brandingSettings.image.bannerExternalUrl
-				covImage.src = imgURL;
+
+function showSearchResult(list,searchUl){
+	let filtered = filterUniqueChannel(list)	
+	//console.log(filtered)
+	searchUl.textContent=""
+	for(item of filtered){
+		console.log(item.snippet.thumbnails.default.url)
+		let li=document.createElement("li")
+		li.data=item.snippet.channelId
+		li.addEventListener("click",getChannel)
+		let img=document.createElement("img")
+		let channelName=document.createElement("h3")
+		img.src=item.snippet.thumbnails.default.url
+		channelName.textContent=`${item.snippet.channelTitle}`
+		li.appendChild(img)
+		li.appendChild(channelName)
+		searchUl.appendChild(li)
+		
+	}	
+}
+async function getChannel(e){
+	fetch(`https://www.googleapis.com/youtube/v3/channels?part=brandingSettings&id=${e.target.data}&key=${youtube-key}`)
+		.then(response => response.json())
+		.then(data => {	
+		 //let	imgURL = data["items"][0].brandingSettings.image.bannerExternalUrl
+				//covImage.src = imgURL;
+				console.log(data)
 			})
 }
-getChannelId();
+//filters result search based on channelTitle
+function filterUniqueChannel(list){
+	let channels={}
+	var filtered = list.filter(obj => !channels[obj.snippet.channelTitle] && (channels[obj.snippet.channelTitle] = true))
+	return filtered
+}
+
 
 
